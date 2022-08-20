6def73ee4d3be82eea3bed89616a8bdaf0618958
AleCamp98 <106559854+AleCamp98@users.noreply.github.com>
2022-08-20 14:02:50 +0200

    Update ram_modified.c

--- ram_modified.c
+++ ram_modified.c
@@ -1,4 +1,79 @@
 /*
+ * Copyright (c) 2000, 2001, 2002, 2003, 2004, 2005, 2008, 2009
+ *	The President and Fellows of Harvard College.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE UNIVERSITY AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE UNIVERSITY OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <types.h>
+#include <lib.h>
+#include <vm.h>
+#include <mainbus.h>
+
+
+vaddr_t firstfree;   /* first free virtual address; set by start.S */
+
+static paddr_t firstpaddr;  /* address of first free physical page */
+static paddr_t lastpaddr;   /* one past end of last free physical page */
+
+/*
+ * Called very early in system boot to figure out how much physical
+ * RAM is available.
+ */
+void ram_bootstrap(void)
+{
+	size_t ramsize;
+
+	/* Get size of RAM. */
+	ramsize = mainbus_ramsize();
+
+	/*
+	 * This is the same as the last physical address, as long as
+	 * we have less than 512 megabytes of memory. If we had more,
+	 * we wouldn't be able to access it all through kseg0 and
+	 * everything would get a lot more complicated. This is not a
+	 * case we are going to worry about.
+	 */
+	if (ramsize > 512*1024*1024) {
+		ramsize = 512*1024*1024;
+	}
+
+	lastpaddr = ramsize;
+
+	/*
+	 * Get first free virtual address from where start.S saved it.
+	 * Convert to physical address.
+	 */
+	firstpaddr = firstfree - MIPS_KSEG0;
+
+	kprintf("%uk physical memory available\n",(lastpaddr-firstpaddr)/1024);
+}
+
+/*
+ * This function is for allocating physical memory prior to VM
+ * initialization.
  *
  * The pages it hands back will not be reported to the VM system when
  * the VM system calls ram_getsize(). If it's desired to free up these
@@ -14,20 +89,20 @@
  * This function should not be called once the VM system is initialized,
  * so it is not synchronized.
  */
-paddr_t         /*ALLOCO UNA PAGINA IN MEMORIA*/
-ram_stealmem(void) /*funzione che alloca spazio contiguo nella memoria fisica. Alloco una pagina per volta*/
+/*Funzione che alloca spazio contiguo nella memoria fisica. Alloco una pagina per volta*/
+paddr_t ram_stealmem(paddr_t firstpaddr1) 
 {
         size_t size;
         paddr_t paddr;
 
         size = PAGE_SIZE;
 
-        if (firstpaddr + size > lastpaddr) {
+        if (firstpaddr1 + size > lastpaddr) {
                 return 0;
         }
 
-        paddr = firstpaddr; /*padd = physical addres, firstpaddr = first physical address*/
-        firstpaddr += size;
+        paddr = firstpaddr1; /*padd = physical addres, firstpaddr = first physical address*/
+        firstpaddr1 += size;
 
         return paddr;
 }
@@ -74,4 +149,3 @@ ram_getfirstfree(void)
         firstpaddr = lastpaddr = 0;
         return ret;
 }
-
