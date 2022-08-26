c0d88040f31471bf585573608702330790d5259a
wtess <wtessitore@gmail.com>
2022-08-26 20:48:21 +0200

    Updated pt.c/.h

--- pt.c
+++ pt.c
@@ -1,60 +1,15 @@
-#include <types.h>
 #include <pt.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <kern/errno.h>
-#include <lib.h>
 
-
-
-struct pgtblstruct *pgtblgen(paddr_t lastpaddr,paddr_t firstpaddr,unsigned long npages)
+struct pt * pt_create(size_t nentries)
 {
-		//Decido che il primo indirizzo di memoria dedicato alla page table sia 64 indirizzi prima(NUM_TLB) dell'ultimo indirizzo disponibile massimo per i frame fisici.
-		firstpaddrpgtbl = lastpaddr - NUM_TLB;
-		lastpaddrpgtbl = lastpaddr; //Mentre l'ultimo indirizzo corrisponde esattamente all'ultimo indirizzo possibile allocato per i frame.
-
-		//if (firstpaddrpagetable + size > lastpaddr)  return 0; //Se il primo indirizzo della page table allocata
-		//nello spazio dei frame è maggiore dell'ultimo indirizzo possibile della memoria allocata per i frame
-		//esce dalla funzione(DA VEDERE SE SI DEVE INVECE CHIAMARE KASSERT O GENERARE QUALCHE ERRORE)
-        
-		//Adesso associo ad ogni indirizzo di memoria della page table una struttura:
-		//Così ho delle strutture per ogni indirizzo per cui so che ogni indirizzo della memoria fisica
-		//per la page table a quale indice appartiene e di conseguenza posso risalire facilmente alle entry
-		//perchè so tutti gli indirizzi.
-
-		struct pgtblstruct *pgtblinfo;
-
-        pgtblinfo = kmalloc(NUM_TLB*(sizeof(struct pgtblstruct))); //alloco il vettore di strutture (64 strutture) in memoria
-        //QUI DEVO CAPIRE COME POSIZIONARE TUTTE LE STRUTTURE NELLA MEMORIA DEDICATA AI FRAMES
-         //Voglio posizionare l'indirizzo della prima struttura nella porzione di memoria da me stabilità all'interno dei frame allocati,
-         //in questo caso vorrei che il mio vettore di strutture cominci dall'indirizzo firstpaddrpgtbl
-        
-		for(int i=0;i<NUM_TLB;i++)
-		{
-			pgtblinfo[i].pgtbl_entry_address = firstpaddrpgtbl+i;
-			//pgtblinfo[i]=pgtblinfo[i].pgtbl_entry_address
-			pgtblinfo[i].valid_invalid_indx = 0; //Decido di mettere a 0 inizialmente tutti i valid-invalid
-			//bit per stabilire se il frame sia in memoria o no
-			//Valid address -->valid_invalid bit = 1
-			//Invalid address -->valid_invalid bit = 0
-		}
-
-		for(int i=0;i<npages;i++)
-		{
-			if(i <= NUM_TLB) //Perchè il numero di pagine potrebbe essere maggiore del numero di entry della TLB 
-			pgtblinfo[i].pgtbl_entry_content = firstpaddr +(i*PAGE_SIZE); //Riempio la page table con tutti 
-			//i primi indirizzi di ogni frame in memoria
-		}
-
-        return pgtblinfo;
-
+    /* simply recalls an allocator from a low-level file
+     * (probably called "userram.c"?).
+     */
 }
 
-
-struct pgtblstruct *getactualpagetable()
+void pt_destroy(struct pt * pt_dest)
 {
-	struct pgtblstruct *actualpgtbl;
-	/* QUI DEVO CAPIRE COME PRELEVARE L'ATTUALE STRUTTURA DELLA PAGE TABLE */
-
-	return actualpgtbl;
+    /* an interface to a deallocator from a low-level file, 
+     * as pt_create. 
+     */
 }
--- pt.h
+++ pt.h
@@ -1,25 +1,18 @@
+#ifndef _PT_H_
+#define _PT_H_
+
 #include <types.h>
-#include <vm.h>
-#include <tlb.h>
-#include <stdio.h>
-#include <stdlib.h>
 
-/*GLOBAL VARIABLES DEFINITION*/
-
-struct pgtblstruct
+/* data structure representing a page table */
+struct pt
 {
-	paddr_t pgtbl_entry_address;
-	paddr_t pgtbl_entry_content;
-	short int valid_invalid_indx;
+    paddr_t frame;
+    bool valid;
 };
 
-paddr_t firstpaddrpgtbl; 
-paddr_t lastpaddrpgtbl;
+/* functions to alloc/dealloc a page table */
+struct pt * pt_create(size_t nentries);
+void pt_destroy(struct pt * pt_dest);
 
+#endif /* _PT_H_ */
 
-/*FUNCTIONS DEFINITION*/
-
-struct pgtblstruct *pgtblgen(paddr_t lastpaddr,paddr_t firstpaddr,unsigned long npages); //Questa funzione genera
-//e ritorna la struttura dati completa necessaria a gestire tutta la page table
-
-struct pgtblstruct *getactualpagetable();
