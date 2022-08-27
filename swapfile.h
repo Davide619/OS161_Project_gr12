66a00c7579e090368e8bd6a4a10427c81ac97034
Davide619 <94796969+Davide619@users.noreply.github.com>
2022-08-27 18:53:05 +0200

    Add files via upload

---
+++ swapfile.h
@@ -0,0 +1,41 @@
+////////////////////////////////////////////////////////////
+//
+// swap
+//
+
+/*
+ * swapfile operations in swap.c:
+ *
+ * swap_bootstrap:   bootstraps the swapfile and completes VM-system 
+ *                   bootstrap. Declared in vm.h.
+ *
+ * swap_shutdown:    closes the swapfile vnode. Declared in vm.h.
+ * 
+ * swap_alloc:       finds a free swap page and marks it as used.
+ *                   A page should have been previously reserved.
+ *
+ * swap_free:        unmarks a swap page.
+ *
+ * swap_pagein:      Reads a page from the requested swap address 
+ *                   into the requested physical page.
+ *
+ * swap_pageout:     Writes a page to the requested swap address 
+ *                   from the requested physical page.
+ */
+
+off_t	 	swap_alloc(paddr_t pa_mem);
+void 		swap_free(off_t swapaddr);
+
+void 		swap_pagein(struct addrspace *as, paddr_t pa_mem, off_t swapaddr);
+void 		swap_pageout(struct addrspace *as, paddr_t pa_mem, off_t swapaddr);
+
+/*
+ * Special disk address:
+ *   INVALID_SWAPADDR is an invalid swap address.
+ */
+#define INVALID_SWAPADDR	(0)
+
+
+//extern struct lock *global_paging_lock;
+
+////////////////////////////////////////////////////////////
