b7c78e7023e54ffd58226b05a3d2576f388cb296
wtess <wtessitore@gmail.com>
2022-08-27 12:48:07 +0200

    Updated coremap.c/.h

--- coremap.c
+++ coremap.c
@@ -5,12 +5,12 @@ struct ffl * ffl_create(const uint8_t nframes)
 
 }
 
-void init(struct ffl * ffl_init)
+void init(struct ffl ** ffl_init, uint8_t nframes)
 {
 
 }
 
-void push(struct ffl * ffl_push, const paddr_t * fr_push)
+void push(struct ffl ** ffl_push, const paddr_t fr_push)
 {
     /* verify if there is necessity of some sort of
      * boundary checking, to avoid push/pop outside che 
@@ -19,15 +19,15 @@ void push(struct ffl * ffl_push, const paddr_t * fr_push)
      * if everything is done properly during allocation of
      * PAGE_SIZE frames, this check should not be necessary.
      */
-    ffl_push = ffl_push->next;
-    ffl_push->free_frame = fr_push;
+    *ffl_push = (*ffl_push)->next;
+    (*ffl_push)->free_frame = fr_push;
 }
 
-paddr_t pop(struct ffl * ffl_pop)
+paddr_t pop(struct ffl ** ffl_pop)
 {
-    paddr_t fr_pop = ffl_pop->free_frame;
-    ffl_pop->free_frame = NULL;
-    ffl_pop = ffl_pop->prev;
+    paddr_t fr_pop = (*ffl_pop)->free_frame;
+    (*ffl_pop)->free_frame = NULL;
+    *ffl_pop = (*ffl_pop)->prev;
 
     return fr_pop;
 }
--- coremap.h
+++ coremap.h
@@ -12,29 +12,35 @@ struct ffl
     struct ffl * next, * prev;
 };
 
-/* init(): function to initialize a ffl struct, it realizes a stack with 
- *         "nframes" entries and sets for each of them the corresponding 
- *         free_frame; this configuration is typical of the early state of 
- *         a program, so before it starts being executed and all the frames 
- *         are untouched.
- *         for each entry, prev is set to "ffl-sizeof(ffl)" (except the first),
- *         basically the previous entry with respect to the current one, while
- *         next is set to "ffl+sizeof(ffl)" (except the last).
+/* ffl_create():  function to create a ffl struct, it calls and internal allocator to
+ *                reserve the proper space in memory, then realizes a stack with "nframes"
+ *                entries and sets for each of them the corresponding relations between
+ *                prev/next pointers, finally, it returns a pointer to the stack base
+ *                in order to allow init() to fill it with the frame addresses.
+ *                for each entry, prev is set to "ffl-sizeof(ffl)" (except the first),
+ *                basically the previous entry with respect to the current one, while
+ *                next is set to "ffl+sizeof(ffl)" (except the last).
  * 
- * push(): function to push a new token inside the stack, first it assignes to 
- *         ffl_push the content of next so that the ffl pointer points to the 
- *         next entry, then it updates free_frame with the content of fr_push.
+ * init():        function to initialize a ffl struct, it takes as input a ffl struct
+ *                pointer and calls push() "nframes" times in order to fill it with 
+ *                the frame addresses.
  * 
- * pop():  function to pop an unused token from the stack, first it saves the
- *         frame of ffl_pop in fr_pop, then it deletes the content of ffl_pop
- *         and finally it updates ffl_pop with prev so that ffl_pop points to
- *         the previous entry.
+ * push():        function to push a new token inside the stack, first it assignes to 
+ *                ffl_push the content of next so that the ffl pointer points to the 
+ *                next entry, then it updates free_frame with the content of fr_push.
+ *
+ * pop():         function to pop an unused token from the stack, first it saves the
+ *                frame of ffl_pop in fr_pop, then it deletes the content of ffl_pop
+ *                and finally it updates ffl_pop with prev so that ffl_pop points to
+ *                the previous entry.
+ *
+ * ffl_destroy():  
  */
 
 struct ffl * ffl_create(const uint8_t nframes);
-void init(struct ffl * ffl_init);
-void push(struct ffl * ffl_push, const paddr_t * fr_push);
-paddr_t pop(struct ffl * ffl_pop);
+void init(struct ffl ** ffl_init, uint8_t nframes);
+void push(struct ffl ** ffl_push, const paddr_t fr_push);
+paddr_t pop(struct ffl ** ffl_pop);
 void ffl_destroy(struct ffl * ffl_dest);
 
 #endif /* _COREMAP_H_ */
