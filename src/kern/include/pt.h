9b02bbde7593d2844628754ce71611fcf3a9a5f7
Alessandro C <106559854+AleCamp98@users.noreply.github.com>
2022-10-03 19:08:21 +0200

    Update pt.h

--- src/kern/include/pt.h
+++ src/kern/include/pt.h
@@ -5,8 +5,18 @@
 #include <lib.h>
 #include <vm.h>
 
+/*Define*/
+
+#define FRAME_NUMBER 0xfffff000
+
 /* functions to alloc/dealloc a page table */
 paddr_t * pt_create(size_t nentries);
 void pt_destroy(paddr_t * pt_dest);
+
+/* functions to Manage page table and replacement*/ 
+paddr_t victim_sel(paddr_t *pt,uint8_t *entry_valid,/*,uint8_t nvalidentries,*/uint8_t index); //<--added
+void pt_update(paddr_t *pt,uint8_t *entry_valid,paddr_t new_frame,uint8_t index,uint8_t pt_index,uint8_t dirty_bit,uint8_t valid_bit);
+
+
 
 #endif /* _PT_H_ */
