a7024d5317c7231072f67a15c81225dee2743144
Alessandro C <106559854+AleCamp98@users.noreply.github.com>
2022-10-05 23:28:24 +0200

    Update pt.c

--- src/kern/vm/pt.c
+++ src/kern/vm/pt.c
@@ -1,5 +1,7 @@
 #include <pt.h>
 
+static uint8_t index;
+
 paddr_t * pt_create(size_t nentries)
 {
     paddr_t * new_pt = kmalloc(sizeof(paddr_t) * nentries);
@@ -15,26 +17,22 @@ void pt_destroy(paddr_t * pt_dest)
     return;
 }
 
-/*Questa funzione,tramite seleziona una vittima tra le entry della page table da rimpiazzare 
+/*Questa funzione,tramite index seleziona una vittima tra le entry della page table da rimpiazzare 
 secondo un algoritmo di replacement*/
-paddr_t victim_sel(paddr_t *pt,uint8_t *entry_valid,/*,uint8_t nvalidentries,*/uint8_t index)
+paddr_t get_victim(paddr_t *pt,uint8_t *entry_valid)
 {
-        
-        paddr_t old_frame;
-
-        old_frame = ((pt[entry_valid[index]] && FRAME_NUMBER))>>12;
-
-        return old_frame;
-
+        return pt[entry_valid[index]];
 }
 
 /*Questa funzione,aggiorna una entry della page table e contemporaneamente un elemento del vettore entry valid:
 - entry_valid è il vettore tiene conto delle sole pagine caricate in memoria (valide) in modo da rispettare il DEMAND PAGING.*/
-void pt_update(paddr_t *pt,uint8_t *entry_valid,paddr_t new_frame,uint8_t index,uint8_t pt_index,uint8_t dirty_bit,uint8_t valid_bit)
+void pt_update(paddr_t *pt,uint8_t *entry_valid,paddr_t new_frame,uint8_t nvalidentries,uint8_t pt_index)
 {
     
     entry_valid[index]==pt_index;
 
-    pt[entry_valid[index]] = new_frame | dirty_bit | valid_bit;
+    pt[entry_valid[index]] = new_frame;
+    
+    index = ((index + 1) == nvalidentries) ? 0 : (index+1);
 
 }
