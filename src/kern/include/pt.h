5b0e135fefd49033b7dba281d99a0438c438a526
Alessandro C <106559854+AleCamp98@users.noreply.github.com>
2022-10-10 19:39:56 +0200

    Update pt.h

--- src/kern/include/pt.h
+++ src/kern/include/pt.h
@@ -13,7 +13,7 @@ void pt_destroy(paddr_t * pt_dest);
 
 /* functions to Manage page table and replacement*/ 
 paddr_t get_victim_frame(paddr_t *pt,uint8_t *entry_valid);
-uint8_t get_victim_page_number(uint8_t *entry_valid);
+uint8_t get_victim_pt_index(uint8_t *entry_valid);
 vaddr_t get_page_number(vaddr_t virtualaddr,uint8_t *entry_valid);
 void pt_update(paddr_t *pt,uint8_t *entry_valid,paddr_t new_frame,uint8_t old_pt_index,uint8_t nvalidentries,uint8_t pt_index)
 
